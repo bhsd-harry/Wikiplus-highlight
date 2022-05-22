f2d70879b920e534f259155159347f6fc7fe833c
bhsd <2545473905@qq.com>
2022-05-22 11:21:20 -0700

    doc: npm package name

--- bump.sh
+++ bump.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 sed -i '' -E "s/version = '.+'/version = '$1'/" main.js
-perl -pi -e "s|Wikiplus-highlight@\d+\..+?/|Wikiplus-highlight\@$1/|" README.md
+perl -pi -e "s|wikiplus-highlight@\d+\..+?/|wikiplus-highlight\@$1/|" README.md
 for file in i18n/*
 do
 	sed -i '' -E "s/\"wphl-version\": \".+\"/\"wphl-version\": \"$1\"/" $file
