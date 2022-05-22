826fa58d805e956a9b305b79b9d0f938f0c132bb
bhsd <2545473905@qq.com>
2022-05-22 11:23:25 -0700

    build(main.js): download as npm package

--- main.js
+++ main.js
@@ -94,7 +94,7 @@
 	const CDN = '//fastly.jsdelivr.net',
 		CM_CDN = 'npm/codemirror@5.65.3',
 		MW_CDN = 'gh/bhsd-harry/codemirror-mediawiki@1.0',
-		REPO_CDN = `gh/bhsd-harry/Wikiplus-highlight@${majorVersion}`;
+		REPO_CDN = `npm/wikiplus-highlight@${majorVersion}`;
 
 	// mw.config常数
 	const {
