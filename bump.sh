2c4746d02579bb3b00482867081c44b900a44885
bhsd <2545473905@qq.com>
2022-05-27 09:08:37 -0700

    feat(bump.sh): switch to jsdelivr gh if only publishing to GitHub

--- bump.sh
+++ bump.sh
@@ -1,6 +1,8 @@
 #!/usr/local/bin/bash
 if [[ $2 == 'npm' ]]
 then
+	sed -i '' 's|gh/bhsd-harry/Wikiplus-highlight@|npm/wikiplus-highlight@|' main.js
+	uglifyjs main.js -c -m --comments --source-map 'url=main.min.js.map,root="../"' -o dist/main.min.js
 	perl -pi -e "s|wikiplus-highlight@\d+\..+?(['/])|wikiplus-highlight\@$1\$1|" README.md
 	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
 	git add -A
@@ -9,7 +11,7 @@ then
 	npm publish
 else
 	sed -i '' -E "s/version = '.+'/version = '$1'/" main.js
-	uglifyjs main.js -c -m --comments --source-map 'url=main.min.js.map,root="../"' -o dist/main.min.js
+	sed -i '' 's|npm/wikiplus-highlight@|gh/bhsd-harry/Wikiplus-highlight@|' main.js
 	for file in i18n/*
 	do
 		sed -i '' -E "s/\"wphl-version\": \".+\"/\"wphl-version\": \"$1\"/" $file
