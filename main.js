0a0e3e14b7d993463a516be3ef9be283bdd8c2f4
bhsd <2545473905@qq.com>
2022-08-23 09:18:11 -0700

    chore: bump version to 2.20

--- main.js
+++ main.js
@@ -13,7 +13,7 @@
 	}
 	mw.libs.wphl = {}; // 开始加载
 
-	const version = '2.19',
+	const version = '2.20',
 		newAddon = 0;
 
 	/** @type {typeof mw.storage} */
@@ -105,7 +105,7 @@
 	const CDN = '//fastly.jsdelivr.net',
 		CM_CDN = 'npm/codemirror@5.65.3',
 		MW_CDN = 'gh/bhsd-harry/codemirror-mediawiki@1.1.5',
-		REPO_CDN = `npm/wikiplus-highlight@${majorVersion}`;
+		REPO_CDN = `gh/bhsd-harry/Wikiplus-highlight@${majorVersion}`;
 
 	const {
 		wgPageName: page,
