8cb6648ebf55f7534ef13bc1f80b974f2b37b048
bhsd <2545473905@qq.com>
2023-01-28 23:54:34 -0800

    chore: publish 2.37.2 to npm

--- main.js
+++ main.js
@@ -100,7 +100,7 @@
 		CM_CDN = 'npm/codemirror@5.65.3',
 		MW_CDN = 'gh/bhsd-harry/codemirror-mediawiki@1.1.6',
 		PARSER_CDN = 'gh/bhsd-harry/wikiparser-node@0.6.18-b',
-		REPO_CDN = `gh/bhsd-harry/Wikiplus-highlight@${majorVersion}`;
+		REPO_CDN = `npm/wikiplus-highlight@${majorVersion}`;
 
 	const {config: {values: {
 		wgPageName: page,
