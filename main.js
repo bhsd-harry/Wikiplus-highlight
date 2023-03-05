01bd29c2473558d06fc277b205f4de777d53f818
bhsd <2545473905@qq.com>
2023-03-05 14:24:01 -0800

    chore: bump version to 2.55.1

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 	}
 	mw.libs.wphl = mw.libs.wphl || {}; // 开始加载
 
-	const version = '2.55',
+	const version = '2.55.1',
 		newAddon = 0;
 
 	/** @type {typeof mw.storage} */
@@ -100,7 +100,7 @@
 	const CDN = '//fastly.jsdelivr.net',
 		CM_CDN = 'npm/codemirror@5.65.3',
 		MW_CDN = 'gh/bhsd-harry/codemirror-mediawiki@1.1.6',
-		PARSER_CDN = 'npm/wikiparser-node@0.9.1-b',
+		PARSER_CDN = 'npm/wikiparser-node@0.9.2-b',
 		REPO_CDN = `gh/bhsd-harry/Wikiplus-highlight@${majorVersion}`;
 
 	const {config: {values: {
