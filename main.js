7da5821696d0618f03002861959c6aef488f544c
bhsd <2545473905@qq.com>
2023-03-07 20:38:09 -0800

    chore: bump version to 2.57.2

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
 	}
 	mw.libs.wphl = mw.libs.wphl || {}; // 开始加载
 
-	const version = '2.57.1',
+	const version = '2.57.2',
 		newAddon = 0;
 
 	/** @type {typeof mw.storage} */
@@ -100,8 +100,8 @@
 	const CDN = '//fastly.jsdelivr.net',
 		CM_CDN = 'npm/codemirror@5.65.3',
 		MW_CDN = 'gh/bhsd-harry/codemirror-mediawiki@1.1.6',
-		PARSER_CDN = 'npm/wikiparser-node@0.9.2-b',
-		REPO_CDN = `npm/wikiplus-highlight@${majorVersion}`;
+		PARSER_CDN = 'gh/bhsd-harry/wikiparser-node@0.9.3-b',
+		REPO_CDN = `gh/bhsd-harry/Wikiplus-highlight@${majorVersion}`;
 
 	const {config: {values: {
 		wgPageName: page,
