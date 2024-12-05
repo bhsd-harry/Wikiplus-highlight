5221d7520a94e17f81c59a2d4ea7d6a0d34a572c
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2024-12-05 21:27:17 +0800

    chore(bump.sh): update

--- bump.sh
+++ bump.sh
@@ -1,16 +1,13 @@
 #!/usr/local/bin/bash
 if [[ $2 == 'npm' ]]
 then
-	sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
-	sed -i '' -E "s/version = '.+'/version = '$1'/" src/main.ts
-	npm run build
-	git add -A
-	git commit -m "chore: publish $1 to npm"
 	npm publish --tag ${3-latest}
 else
-	npm run lint
+	npm run lint && npm run build
 	if [[ $? -eq 0 ]]
 	then
+		sed -i '' -E "s/\"version\": \".+\"/\"version\": \"$1\"/" package.json
+		sed -i '' -E "s/version = '.+'/version = '$1'/" src/main.ts
 		git add -A
 		git commit -m "chore: bump version to $1"
 		git push
