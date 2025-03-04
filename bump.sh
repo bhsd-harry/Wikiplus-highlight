b1a91eea3a635a229b4dfbef4f3a089ea39ca84b
Bhsd <55071315+bhsd-harry@users.noreply.github.com>
2025-03-05 05:32:20 +0800

    chore(package.json): update

--- bump.sh
+++ bump.sh
@@ -1,7 +1,7 @@
 #!/usr/local/bin/bash
 if [[ $2 == 'npm' ]]
 then
-	npm publish --tag ${3-latest}
+	npm publish --tag "${3-latest}"
 else
 	npm run lint && npm run build
 	if [[ $? -eq 0 ]]
@@ -11,7 +11,7 @@ else
 		git add -A
 		git commit -m "chore: bump version to $1"
 		git push
-		git tag $1
-		git push origin $1
+		git tag "$1"
+		git push origin "$1"
 	fi
 fi
